f8e0114a11a1653d0923ba92192dbb95c40a9c06
Albert Sunyer <asunyer@ooptimo.com>
2024-01-16 10:59:24 +0100

    v1

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+'use strict'
+
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')(require('node-sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemaps = require('gulp-sourcemaps')
+const cleanCSS = require('gulp-clean-css')
+
+const main_css = () =>
+	gulp
+	.src(['./src/scss/**/*.scss'])
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(rename('main.css'))
+	.pipe(cleanCSS())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('./css/'))
+
+const watch = () => {
+	gulp.watch(['./src/scss/main.scss'], gulp.series('main_css'))
+}
+
+exports.main_css = main_css
+exports.watch = watch
+
+const dev = gulp.series(main_css, watch)
+
+exports.dev = dev
+exports.default = dev
